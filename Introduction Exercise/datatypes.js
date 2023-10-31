52325b8a7b4f6bb765301ec749b2edd8bf3ac44f
JesusHdzzz <2202039@utrivieramaya.edu.mx>
2023-10-30 19:36:14 -0500

    first commit

---
+++ Introduction Exercise/datatypes.js
@@ -0,0 +1,46 @@
+// Para ejecutar cada ejercicio, elimina "/*" y "*/" para poder revisar el ejercicio. 
+
+/*
+console.log(typeof 'César')
+console.log(typeof 18)
+console.log(typeof null)
+console.log(typeof undefined)
+*/
+
+// Declarar 4 variables sin asignar valores.
+// Área del trapecio
+/*
+let BaseMayor
+let BaseMenor
+let altura
+let resul
+*/
+
+// Declarar variables asignando valores.
+// Datos personales
+/*
+let name = 'Nasly'
+let lastName = 'Arcos'
+let age = '19'
+let weight = '80'
+*/
+
+// Declarar variables asignando mi nombre.
+/*
+let nombre = 'Jesús'
+let apellido = 'Hernandez'
+let EstadoCivil = 'Soltero'
+let país = 'México'
+let edad = 18
+*/
+
+// Declarar las variables anteriores en una sola línea.
+// let nombre = 'Jesús', apellido = 'Hernandez', EstadoCivil = 'Soltero', país = 'México', edad = 18
+
+// Declara variables "myAge" y "your Age" y asignar valores iniciales.
+
+let myAge = 18
+let yourAge = 19
+
+console.log('Mi edad es: '+ myAge)
+console.log('Tu edad es: '+yourAge)
---
+++ Introduction Exercise/variable.js
@@ -0,0 +1,11 @@
+// Los comentarios pueden hacer que el código sea legible.
+// Este es mi primer código JS.
+// 
+// Los comentarios pueden hacer que el código sea legible,
+// fácil de reutilizar e informativo
+//
+
+let name = 'Jesús'
+let student = true
+let age = undefined
+let address = null
